c0b913a40e0cb26d7b39ed77406d10b71df4ab49
loovjo <jonathan.loov@gmail.com>
2017-05-14 19:19:21 +0200

    Added elm to the Install.py file

--- Install.py
+++ Install.py
@@ -26,6 +26,22 @@ if not command_exists("node"):
         print("nodejs not found, trying node.js instead...")
         inst = subprocess.Popen((PACKAGE + " node.js").split())
 
+if not command_exists("elm"):
+    if len(sys.argv) < 2:
+        print("Run as python3 Make.py <package manager>")
+        sys.exit(1)
+
+    PACKAGE = " ".join(sys.argv[1:])
+    if not command_exists(PACKAGE.split(" ")[0]):
+        print("Can't find package manager %s. Some common ones to try are brew install (mac), apt-get install (most linux distros)")
+        sys.exit(1)
+    print("Elm not installed, install it?")
+    res = input("[Y/n]")
+    if res == "n":
+        print("Cancelling installation")
+        sys.exit(1)
+    inst = subprocess.Popen((PACKAGE + " elm").split())
+
 for module in "ws", "ip":
     a = subprocess.Popen(["npm", "ls", module], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     errorCode = a.wait()
