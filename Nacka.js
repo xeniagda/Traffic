ffb4fe8ef75e7df83db2b6dcf825cd28e5b14188
loovjo <jonathan.loov@gmail.com>
2017-05-18 20:04:07 +0200

    Added little new part in Nacka.js

--- Nacka.js
+++ Nacka.js
@@ -263,7 +263,9 @@
                 "x": -23,
                 "y": -16
             },
-            "connected_to": [],
+            "connected_to": [
+                "f4ux7i0nb29f5p0n3s74"
+            ],
             "speed_rec": 5
         },
         {
@@ -301,6 +303,373 @@
                 "offset": 1,
                 "at": 1
             }
+        },
+        {
+            "id": "f4ux7i0nb29f5p0n3s74",
+            "width": 1.5,
+            "start": {
+                "x": -23,
+                "y": -16
+            },
+            "end": {
+                "x": -27,
+                "y": -48
+            },
+            "connected_to": [
+                "oginehr7hq0il5dsj6jj"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "80br0bk6oleongy6n0nn",
+            "width": 1.5,
+            "start": {
+                "x": -29,
+                "y": -47
+            },
+            "end": {
+                "x": -25,
+                "y": -16
+            },
+            "connected_to": [
+                "ir1990uang9c5yh73niy"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "s8e7d9mblzoiotaro6n8",
+            "width": 1.5,
+            "start": {
+                "x": -28,
+                "y": -74
+            },
+            "end": {
+                "x": -29,
+                "y": -47
+            },
+            "connected_to": [
+                "80br0bk6oleongy6n0nn"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "oginehr7hq0il5dsj6jj",
+            "width": 1.5,
+            "start": {
+                "x": -27,
+                "y": -48
+            },
+            "end": {
+                "x": -26,
+                "y": -74
+            },
+            "connected_to": [
+                "cypkpevvmx4vlmbzz8oj"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "cypkpevvmx4vlmbzz8oj",
+            "width": 1.5,
+            "start": {
+                "x": -26,
+                "y": -74
+            },
+            "end": {
+                "x": -24,
+                "y": -83
+            },
+            "connected_to": [
+                "vvedz24trgnaiuwnk2rp"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "hp5svzl2h1y77auvahig",
+            "width": 1.5,
+            "start": {
+                "x": -27,
+                "y": -84
+            },
+            "end": {
+                "x": -28,
+                "y": -74
+            },
+            "connected_to": [
+                "s8e7d9mblzoiotaro6n8"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "vvedz24trgnaiuwnk2rp",
+            "width": 1.5,
+            "start": {
+                "x": -24,
+                "y": -83
+            },
+            "end": {
+                "x": -22,
+                "y": -84
+            },
+            "connected_to": [
+                "tbvktvqjscsaf7rtzrzo"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "tbvktvqjscsaf7rtzrzo",
+            "width": 1.5,
+            "start": {
+                "x": -22,
+                "y": -84
+            },
+            "end": {
+                "x": -20,
+                "y": -87
+            },
+            "connected_to": [
+                "787abyr6d6wmj9za31vy",
+                "5qk2itny4jyf1n6p3tw9"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "787abyr6d6wmj9za31vy",
+            "width": 1.5,
+            "start": {
+                "x": -20,
+                "y": -87
+            },
+            "end": {
+                "x": -21,
+                "y": -91
+            },
+            "connected_to": [
+                "ylj3hfh6okvu9gvhn5hb"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "ylj3hfh6okvu9gvhn5hb",
+            "width": 1.5,
+            "start": {
+                "x": -21,
+                "y": -91
+            },
+            "end": {
+                "x": -26,
+                "y": -92
+            },
+            "connected_to": [
+                "r0gm8fqtkx1yycfw8l5v",
+                "np7taaz7jtyo77vb0c1b"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "r0gm8fqtkx1yycfw8l5v",
+            "width": 1.5,
+            "start": {
+                "x": -26,
+                "y": -92
+            },
+            "end": {
+                "x": -29,
+                "y": -90
+            },
+            "connected_to": [
+                "xcygcs6s9qbj7380qe1b"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "xcygcs6s9qbj7380qe1b",
+            "width": 1.5,
+            "start": {
+                "x": -29,
+                "y": -90
+            },
+            "end": {
+                "x": -29,
+                "y": -86
+            },
+            "connected_to": [
+                "llh8xxcc7472gxchkf6b"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "llh8xxcc7472gxchkf6b",
+            "width": 1.5,
+            "start": {
+                "x": -29,
+                "y": -86
+            },
+            "end": {
+                "x": -27,
+                "y": -84
+            },
+            "connected_to": [
+                "hp5svzl2h1y77auvahig",
+                "e3nox69vxroj1su5w3pd"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "e3nox69vxroj1su5w3pd",
+            "width": 1.5,
+            "start": {
+                "x": -27,
+                "y": -84
+            },
+            "end": {
+                "x": -24,
+                "y": -83
+            },
+            "connected_to": [
+                "vvedz24trgnaiuwnk2rp"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "np7taaz7jtyo77vb0c1b",
+            "width": 1.5,
+            "start": {
+                "x": -26,
+                "y": -92
+            },
+            "end": {
+                "x": -33,
+                "y": -93
+            },
+            "connected_to": [
+                "em1gi55m0hu0b105zfre"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "em1gi55m0hu0b105zfre",
+            "width": 1.5,
+            "start": {
+                "x": -33,
+                "y": -93
+            },
+            "end": {
+                "x": -37,
+                "y": -92
+            },
+            "connected_to": [
+                "tss17rfctb7lcrtbefio"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "tss17rfctb7lcrtbefio",
+            "width": 1.5,
+            "start": {
+                "x": -37,
+                "y": -92
+            },
+            "end": {
+                "x": -51,
+                "y": -94
+            },
+            "connected_to": [],
+            "speed_rec": 5
+        },
+        {
+            "id": "jcqlrdkfxrdljxmndmlf",
+            "width": 1.5,
+            "start": {
+                "x": -51,
+                "y": -92
+            },
+            "end": {
+                "x": -37,
+                "y": -90
+            },
+            "connected_to": [
+                "gk1mw8jxgx9nh7atdagr"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "gk1mw8jxgx9nh7atdagr",
+            "width": 1.5,
+            "start": {
+                "x": -37,
+                "y": -90
+            },
+            "end": {
+                "x": -31,
+                "y": -88
+            },
+            "connected_to": [
+                "1qntprlku722dr6gedkr"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "1qntprlku722dr6gedkr",
+            "width": 1.5,
+            "start": {
+                "x": -31,
+                "y": -88
+            },
+            "end": {
+                "x": -29,
+                "y": -86
+            },
+            "connected_to": [
+                "llh8xxcc7472gxchkf6b"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "5qk2itny4jyf1n6p3tw9",
+            "width": 1.5,
+            "start": {
+                "x": -20,
+                "y": -87
+            },
+            "end": {
+                "x": -9,
+                "y": -95
+            },
+            "connected_to": [],
+            "speed_rec": 5
+        },
+        {
+            "id": "9uicgf2drwi1eo96pueq",
+            "width": 1.5,
+            "start": {
+                "x": -16,
+                "y": -92
+            },
+            "end": {
+                "x": -21,
+                "y": -91
+            },
+            "connected_to": [
+                "ylj3hfh6okvu9gvhn5hb"
+            ],
+            "speed_rec": 5
+        },
+        {
+            "id": "kgy35f5tiz10xx1w9sko",
+            "width": 1.5,
+            "start": {
+                "x": -8,
+                "y": -98
+            },
+            "end": {
+                "x": -16,
+                "y": -92
+            },
+            "connected_to": [
+                "9uicgf2drwi1eo96pueq"
+            ],
+            "speed_rec": 5
         }
     ],
     "intersections": [
